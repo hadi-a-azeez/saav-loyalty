3a229461e227b42021d84ac4eb838aaa5029ee0f
hadi-a-azeez <hadikoduvally@gmail.com>
2023-02-16 15:32:51 +0530

    feat: style fix

--- src/lib/constants.tsx
+++ src/lib/constants.tsx
@@ -2,26 +2,26 @@ export const coupons = [
   {
     campaign: 'REGISTRATION',
     coupon: '₹100 Off',
-    description: 'Rs 100 discount on first order',
+    description: 'Rs 100 off as a registration reward',
   },
   {
     campaign: 'REGISTRATION',
     coupon: '₹150 Off',
-    description: 'Rs 150 discount on first order',
+    description: 'Rs 150 off as a registration reward',
   },
   {
     campaign: 'REGISTRATION',
     coupon: '₹200 Off',
-    description: 'Rs 200 discount on first order',
+    description: 'Rs.200 off above 3000 purchase',
   },
   {
     campaign: 'REGISTRATION',
-    coupon: '10% Off',
-    description: '10% discount on purchase of Rs 2000 or more',
+    coupon: '₹500 Off',
+    description: 'Rs.500 off above 5000 purchase',
   },
   {
     campaign: 'REGISTRATION',
-    coupon: '20% Off',
-    description: '25% discount on purchase of Rs 5000 or more',
+    coupon: '15% Off',
+    description: '15% off on above 8000 purchase',
   },
 ];
